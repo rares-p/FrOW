b6d4724394f762b69f08e45454f4ffce895fdaef
maxileo <casuneanu.stefan@yahoo.com>
2023-04-09 23:34:50 +0300

    Changed logo to also take you home

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Button from "./Button.js";
 const Header = () => {
     return `
     <header id="headerContainer">
-        <img id = "logo" src="../src/media/logo.png">
+        <input type="image" id="logo" src="../src/media/logo.png" onclick="location.replace('./menu.html')">
         <h1>Fruits on the web</h1>
         ${Button()}
     </header>
